1cfaa45731cd63a267265debc2499b35870e6e32
Prajit Viswanadha <prajit.viswanadha.06@gmail.com>
2025-05-12 13:55:33 -0700

    debugging

--- appwrite/config.ts
+++ appwrite/config.ts
@@ -2,11 +2,11 @@ import { Client, Account, Databases, Storage } from 'react-native-appwrite';
 import { Platform } from 'react-native';
 
 const client = new Client()
-  .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT || 'http://localhost/v1')
-  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID || '');
+  .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT)
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID);
 
 if (Platform.OS !== 'web') {
-  client.setPlatform(process.env.EXPO_PUBLIC_APPWRITE_PLATFORM || '');
+  client.setPlatform(process.env.EXPO_PUBLIC_APPWRITE_PLATFORM);
 }
 
 const account = new Account(client);
@@ -31,12 +31,12 @@ const getImageUrl = (
   return storage.getFilePreview(bucketId, fileId, width, height).toString();
 };
 
-export const DATABASE_ID = process.env.EXPO_PUBLIC_DATABASE_ID || '67fc14630023511bae40';
-export const USERS_COLLECTION_ID = process.env.EXPO_PUBLIC_USERS_COLLECTION_ID || 'users';
-export const LISTINGS_COLLECTION_ID = process.env.EXPO_PUBLIC_LISTINGS_COLLECTION_ID || 'listings';
-export const IMAGES_COLLECTION_ID = process.env.EXPO_PUBLIC_IMAGES_COLLECTION_ID || 'images';
-export const IMAGES_BUCKET_ID = process.env.EXPO_PUBLIC_IMAGES_BUCKET_ID || 'listing-images';
-export const CHATS_COLLECTION_ID = process.env.EXPO_PUBLIC_CHATS_COLLECTION_ID || '67fc151b00269d9b1221';
-export const MESSAGES_COLLECTION_ID = process.env.EXPO_PUBLIC_MESSAGES_COLLECTION_ID || '67fc151e003681b8ba54';
+export const DATABASE_ID = process.env.EXPO_PUBLIC_DATABASE_ID;
+export const USERS_COLLECTION_ID = process.env.EXPO_PUBLIC_USERS_COLLECTION_ID;
+export const LISTINGS_COLLECTION_ID = process.env.EXPO_PUBLIC_LISTINGS_COLLECTION_ID;
+export const IMAGES_COLLECTION_ID = process.env.EXPO_PUBLIC_IMAGES_COLLECTION_ID;
+export const IMAGES_BUCKET_ID = process.env.EXPO_PUBLIC_IMAGES_BUCKET_ID;
+export const CHATS_COLLECTION_ID = process.env.EXPO_PUBLIC_CHATS_COLLECTION_ID;
+export const MESSAGES_COLLECTION_ID = process.env.EXPO_PUBLIC_MESSAGES_COLLECTION_ID;
 
 export { client, account, databases, storage, getImageUrl };
